306253d7adef15707a766d5de11a26a12d7ec0b7
jarabialex <jarabialex@gmail.com>
2022-01-09 23:58:48 +0300

    perf
    Move DOM manipulation out of the loop so it only runs once

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 let myLeads = []
 const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
+const ulEl = document.querySelector("#ul-el")
 
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value)
+  renderLeads()
 })
+
+function renderLeads() {
+  let listItems = ""
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>${myLeads[i]}</li>`
+  }
+  
+  ulEl.innerHTML = listItems
+}
