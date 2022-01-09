3b589d514e56028dce4dfdf928ff8f989f5c9142
jarabialex <jarabialex@gmail.com>
2022-01-10 00:28:54 +0300

    style
    Added styling to the list of leads

--- index.js
+++ index.js
@@ -12,7 +12,11 @@ inputBtn.addEventListener("click", () => {
 function renderLeads() {
   let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `<li>${myLeads[i]}</li>`
+    listItems += `<li>
+                    <a href="${myLeads[i]}" target="_blank">
+                      ${myLeads[i]}
+                    </a>
+                  </li>`
   }
   
   ulEl.innerHTML = listItems
