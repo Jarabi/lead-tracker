29fd3e2e0c90672c57b06a6ff8c1cde53931749c
jarabialex <jarabialex@gmail.com>
2022-01-11 23:31:53 +0300

    feat
    Added feature to get the current tab URL and store in local storage

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 const deleteBtn = document.querySelector("#delete-btn")
+const tabBtn = document.querySelector("#tab-btn")
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
@@ -10,6 +11,14 @@ if(leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage
   render(myLeads)
 }
+
+tabBtn.addEventListener("click", () => {
+  chrome.tabs.query({active: true, currentWindow: true}, tabs => {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+  })
+})
 
 function render(leads) {
   let listItems = ""
