8df547745acdf79ae940b0d3291209e2b99a687b
jarabialex <jarabialex@gmail.com>
2022-01-11 20:09:08 +0300

    feat
    Add condition to render leads on page if they exist

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ const inputBtn = document.querySelector("#input-btn")
 const inputEl = document.querySelector("#input-el")
 const ulEl = document.querySelector("#ul-el")
 
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
+
+if(Boolean(leadsFromLocalStorage)) {
+  myLeads.push(leadsFromLocalStorage)
+  renderLeads()
+}
+// console.log(leadsFromLocalStorage);
+
 inputBtn.addEventListener("click", () => {
   myLeads.push(inputEl.value)
   inputEl.value = ""
+
+  localStorage.setItem("myLeads", JSON.stringify(myLeads))
   renderLeads()
 })
 
 function renderLeads() {
   let listItems = ""
   for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-      <li>
+    listItems +=
+    `<li>
         <a href="${myLeads[i]}" target="_blank">
           ${myLeads[i]}
         </a>
-      </li>`
+    </li>`
   }
   
   ulEl.innerHTML = listItems
